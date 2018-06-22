4722febf4543b50d2ee90c2946207a41003ba9b3
Falon Darville <falondarville@gmail.com>
2018-06-21 17:06:10 -0700

    set up for auth0

--- components/twitterLogIn.js
+++ components/twitterLogIn.js
@@ -1,8 +1,35 @@
 import React, { Component } from 'react';
 import { View, TouchableOpacity, StyleSheet, Text } from 'react-native';
 import { LinearGradient } from 'expo';
+import { TWITTER_API_KEY } from 'react-native-dotenv'
+import auth0 from 'react-native-auth0';
+
+const auth0 = new Auth0({ domain: 'twitter-ai.auth0.com', clientId: TWITTER_API_KEY });
 
 export default class twitterLogIn extends Component {
+
+	constructor(props) {
+	    super(props);
+	    this.state = { 
+	    	accessToken: null 
+	    };
+	  }
+
+	 _logIn = () => {
+		auth0.webAuth
+	    	.authorize({
+	    		scope: 'openid profile',
+	    		audience: 'https://' + 'twitter-ai.auth0.com' + '/userinfo'
+	    })
+	    .then(credentials =>
+	      console.log(credentials)
+	      // Successfully authenticated
+	      // Store the accessToken
+	      this.setState({ credentials.accessToken });
+	    )
+	    .catch(error => console.log(error));
+	}
+
 	render() {
 		return (
 			<View style={styles.container}>
@@ -11,7 +38,7 @@ export default class twitterLogIn extends Component {
 			          colors={[ '#841584', '#134E5E']}
 			          style={{ padding: 8, alignItems: 'center', borderRadius: 2 }}
 			          start={[ 0.0, 0.5 ]} end={[ 1.0, 0.5 ]} locations={[ 0.0, 1.0 ]}>				
-				<TouchableOpacity accessibilityLabel="Sign in with Twitter" onPress={this.handleSubmit}>
+				<TouchableOpacity accessibilityLabel="Sign in with Twitter" onPress={this._logIn}>
 					<Text style={styles.buttonText}>Sign in with Twitter</Text>
 				</TouchableOpacity>
 				</LinearGradient>
